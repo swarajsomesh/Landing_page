b6de9ffb9a7518025d7669fd0106acca212d8f82
Swaraj <55526319+swarajsomesh@users.noreply.github.com>
2021-02-17 00:53:20 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+const menuToggle = document.querySelector(".toggle");
+const showcaseToggle = document.querySelector(".showcase");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcaseToggle.classList.toggle("active");
+});
